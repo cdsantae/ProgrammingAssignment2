565f8ac3e68eeb6072b65e7d6d8f62f59e1511ee
Cristian Santa <cdsantae@gmail.com>
2015-03-13 09:25:01 -0500

    My cachematrix.R
    This function calculate the inverse of a square Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function make a special "Matrix" and take the cache of the method to
+## calculate the inverse of the matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+   s<-NULL;
+   setMatrix<-function(y)
+   {
+      x<<-y;
+      s<<-NULL;      
+   }
+   getMatrix<-function() x;
+   setInverse<-function(solve) s<<-solve;
+   getInverse<-function() s;
+   list(setMatrix = setMatrix, getMatrix = getMatrix,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function take a Matrix and calculate the inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+   s<-x$getInverse()
+   if(!is.null(s))
+   {
+      message('getting cache matrix data');
+      return(m);
+   }
+   matrix<-x$getMatrix();
+   s<-solve(matrix,...)
+   x$setInverse(s)
+   s
 }
